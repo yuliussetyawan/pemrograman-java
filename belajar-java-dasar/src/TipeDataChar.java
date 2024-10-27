66854a46e3bc1a6225b6a206e5ba37b28751c2c2
yuliussetyawan <yuliusetyawan@gmail.com>
2024-10-27 20:14:37 +0700

    04-07 tipe data number, character, boolean, string

---
+++ belajar-java-dasar/src/TipeDataChar.java
@@ -0,0 +1,11 @@
+public class TipeDataChar {
+    public static void main(String[] args) {
+        char y = 'Y';
+        char u = 'U';
+        char l = 'L';
+
+        System.out.print(y);
+        System.out.print(u);
+        System.out.print(l);
+    }
+}
---
+++ belajar-java-dasar/src/TipeDataString.java
@@ -0,0 +1,12 @@
+public class TipeDataString {
+    public static void main(String[] args) {
+        String firstName = "Yulius";
+        String lastName = "Setyawan";
+
+        String fullName = firstName + " " + lastName;
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(fullName);
+    }
+}
