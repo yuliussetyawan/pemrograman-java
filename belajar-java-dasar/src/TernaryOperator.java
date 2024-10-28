99ffb2d8f4726247e54f6a6ebbc3152d05c348b8
yuliussetyawan <yuliusetyawan@gmail.com>
2024-10-28 11:43:58 +0700

    11-20 operasi matematika, perbandingan, boolean, expression-statement-dan-block, if-statement, switch statement, for, while, do-while loop

---
+++ belajar-java-dasar/src/TernaryOperator.java
@@ -0,0 +1,8 @@
+public class TernaryOperator {
+    public static void main(String[] args) {
+        var nilai = 75;
+        String ucapan = nilai >= 75 ? "Selamat Anda Lulus" : "Silahkan coba lagi";
+
+        System.out.println(ucapan);
+    }
+}
