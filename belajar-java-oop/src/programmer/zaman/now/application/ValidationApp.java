f52f80be17941043b67b0b331c8f20897a42fded
yuliussetyawan <yuliusetyawan@gmail.com>
2024-11-26 22:21:25 +0700

    37 exception

---
+++ belajar-java-oop/src/programmer/zaman/now/application/ValidationApp.java
@@ -0,0 +1,25 @@
+package programmer.zaman.now.application;
+
+
+import programmer.zaman.now.data.LoginRequest;
+import programmer.zaman.now.error.ValidationException;
+import programmer.zaman.now.util.ValidationUtil;
+
+public class ValidationApp {
+    public static void main(String[] args) {
+
+        LoginRequest loginRequest = new LoginRequest("eko", "rahasia");
+
+        try {
+            ValidationUtil.validate(loginRequest);
+            System.out.println("Data valid");
+            // multiple try catch
+        } catch (ValidationException | NullPointerException exception) {
+            System.out.println("Data tidak valid : " + exception.getMessage());
+        } finally {
+            System.out.println("Selalu di eksekusi");
+        }
+
+
+    }
+}
